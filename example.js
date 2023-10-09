8ac990d70d1b20da2e2cf2e0a190a6bc699cbf85
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2023-10-09 13:32:50 -0300

    fixed example

--- example.js
+++ example.js
@@ -1,17 +1,15 @@
 console.clear();
 import { train, generate,getRandomNumber } from "./index.js"
 
+const rule = ([a, b, c]) => [a * 5 + c * b, a + c ]
 
-let [x,y] = [
-    [[1,0,0],[0,1,0],[1,0,1],[0,1,1]],
-    [[1,0],[0,1],[0,1],[1,0]]
-]
+let [x,y] = generate(0, 10, 1000, 3, rule, true)
 
 console.time("training")
-const model = train(x, y)
+const model = train(x, y, 1)
 console.timeEnd("training") // training: 318.604ms
 
 console.log(model.solutions) // [ 'a*5+b*c', 'a+c' ]
 
-console.log(model.predict([0, 1, 0])) // [ 41, 13 ]
-// console.log(rule([5, 2, 8])) // [ 41, 13 ]
+console.log(model.predict([5, 2, 8])) // [ 41, 13 ]
+console.log(rule([5, 2, 8])) // [ 41, 13 ]
